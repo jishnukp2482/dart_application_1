b2bd4289cb457706661be889a2c9245f05b9ad56
jishnukp2482 <jishnuvidyadharan123@gmail.com>
2022-09-19 17:34:37 +0530

    commit

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,9 @@
+import 'package:dart_application_1/dart_application_1.dart'
+    as dart_application_1;
+
+void main() {
+  int a = 10;
+  int b = 5;
+  print(
+      'sum is ${a + b} , difference is ${a - b}, multiple is ${a * b}, divison is ${a / b}');
+}
