3be130f707f4bff970482e9f7e69430b5a3dc86a
jishnukp2482 <jishnuvidyadharan123@gmail.com>
2022-09-21 13:50:20 +0530

    parametrizeddetailseg

---
+++ bin/parametrizedegdeatils.dart
@@ -0,0 +1,13 @@
+void showstudentdetils(String name, int age, String email, int mbno) {
+  print("NAME OF THE STUDENT nnnn :${name}");
+  print("AGE OF THE STUDENT       :${age}");
+  print("EMAIL OF THE STUDENT     :${email}");
+  print("MOBILE NO OF THE STUDENT :${mbno}");
+  print("*******************************************");
+}
+
+void main() {
+  showstudentdetils("JISHNU", 21, "jishnu@gmail.com", 123);
+  showstudentdetils("DEEPESH", 10, "deepu@gmail.com", 987);
+  showstudentdetils("VISHNU", 15, "vishnu@gmail.com", 721);
+}
